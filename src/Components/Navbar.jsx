927b57370d8b8fe46e2795ee8e40222f7ad0e898
rajeshbakhrel <rajeshbakhrel@gmail.com>
2023-12-20 14:22:24 +0545

    road map

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Style/";
+
+const Navbar = () => {
+  return (
+    <div className="NavbarContent">
+      <div className="logo">hello</div>
+      <div className="contentnav">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Book Now</li>
+        <li>Contact Us</li>
+      </div>
+      <div className="login">login</div>
+    </div>
+  );
+};
+
+export default Navbar;
